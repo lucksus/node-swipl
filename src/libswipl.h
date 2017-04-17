747a556aa67652e0062b57fde090b0845a613184
Raivo Laanemets <raivo@infdot.com>
2017-04-17 23:44:57 +0300

    Empty line at the end. Remove extra LGPL 2 headers, we use version 3.

--- src/libswipl.h
+++ src/libswipl.h
@@ -1,26 +1,3 @@
-/*  $Id$
-
-    node.js addon for SWI-Prolog
-
-    Author:        Tom Klonikowski
-    E-mail:        klonik_t@informatik.haw-hamburg.de
-    Copyright (C): 2012, Tom Klonikowski
-
-    This library is free software; you can redistribute it and/or
-    modify it under the terms of the GNU Lesser General Public
-    License as published by the Free Software Foundation; either
-    version 2.1 of the License, or (at your option) any later version.
-
-    This library is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-    Lesser General Public License for more details.
-
-    You should have received a copy of the GNU Lesser General Public
-    License along with this library; if not, write to the Free Software
-    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-*/
-
 #ifndef NODE_LIBSWIPL_H_
 #define NODE_LIBSWIPL_H_
 
