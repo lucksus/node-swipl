a1e3eb69f4bcb8cff562b3bee501b0635818f5c8
Raivo Laanemets <raivo@infdot.com>
2017-04-20 02:27:37 +0300

    Safe way to construct query terms.

--- index.js
+++ index.js
@@ -10,79 +10,87 @@ const plbase = fs.readFileSync(filename, 'utf8').trim();
 process.env.SWI_HOME_DIR = plbase;
 
 let initialised = false;
-let autoInitialise = true;
+let auto = true;
 
 // Allows to disable autoinitialization of
 // the SWI-Prolog engine.
 
-exports.autoInitialise = (initialise) => {
-	autoInitialise = !!initialise;
+const autoInitialise = (initialise) => {
+    auto = !!initialise;
 };
 
 let current = null;
 
-const Query = exports.Query = class Query {
-	constructor(query) {
-		assert.equal(typeof query, 'string',
-			'Query must be set as a string.');
-		assert.ok(current === null,
-			'There can be only one open query at a time.');
-		if (!initialised) {
-			if (autoInitialise) {
-				if (!swipl.initialise('node')) {
-					throw new Error('SWI-Prolog engine initialization failed.');
-				}
-				initialised = true;
-			} else {
-				throw new Error('SWI-Prolog is not initialised.');
-			}
-		}
-		this.internal = new swipl.InternalQuery(query);
-		current = this;
-	}
+class Query {
+    constructor(query) {
+        assert.equal(typeof query, 'string',
+            'Query must be set as a string.');
+        assert.ok(current === null,
+            'There can be only one open query at a time.');
+        if (!initialised) {
+            if (auto) {
+                if (!swipl.initialise('node')) {
+                    throw new Error('SWI-Prolog engine initialization failed.');
+                }
+                initialised = true;
+            } else {
+                throw new Error('SWI-Prolog is not initialised.');
+            }
+        }
+        this.internal = new swipl.InternalQuery(query);
+        current = this;
+    }
 
-	next() {
-		const bindings = this.internal.next();
-		if (!bindings) {
-			return false;
-		} else {
-			return extractBindings(bindings);
-		}
-	}
+    next() {
+        const bindings = this.internal.next();
+        if (!bindings) {
+            return false;
+        } else {
+            return extractBindings(bindings);
+        }
+    }
 
-	close() {
-		this.internal.close();
-		current = null;
-	}
+    close() {
+        this.internal.close();
+        current = null;
+    }
 };
 
 // Helper to call single query.
 
-exports.call = (query) => {
-	const instance = new Query(query);
-	try {
-		const bindings = instance.next();
-		return bindings;
-	} finally {
-		instance.close();
-	}
+const call = (query) => {
+    const instance = new Query(query);
+    try {
+        const bindings = instance.next();
+        return bindings;
+    } finally {
+        instance.close();
+    }
 };
 
 // Extracts bindings from the option list.
 
 const extractBindings = (list) => {
-	const bindings = {};
-	while (list !== '[]') {
-		bindings[list.head.args[0]] = list.head.args[1];
-		list = list.tail;
-	}
-	return bindings;
+    const bindings = {};
+    while (list !== '[]') {
+        bindings[list.head.args[0]] = list.head.args[1];
+        list = list.tail;
+    }
+    return bindings;
 };
 
-exports.initialise = () => {
-	return swipl.initialise('node');
+const initialise = () => {
+    return swipl.initialise('node');
 };
 
-exports.cleanup = function() {
-	return swipl.cleanup();
+const cleanup = () => {
+    return swipl.cleanup();
+};
+
+module.exports = {
+    autoInitialise,
+    Query,
+    call,
+    initialise,
+    cleanup
 };
